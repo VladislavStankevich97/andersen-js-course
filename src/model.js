42cb00e99357872ed03a7d9807d1449f02338617
VladislavStankevich97 <ctahok111@gmail.com.com>
2020-09-13 13:07:39 +0300

    сделал

---
+++ src/model.js
@@ -0,0 +1,109 @@
+import { EventEmitter } from './EventEmitter';
+import Recipe from './Recipe';
+import Item from './Item';
+
+class Model extends EventEmitter {
+  constructor(
+    items = [
+      new Item('item0', 'Молоко'),
+      new Item('item1', 'Банан'),
+      new Item('item2', 'Апельсин'),
+      new Item('item4', 'Сахар'),
+      new Item('item5', 'Лёд'),
+      new Item('item6', 'Лайм'),
+      new Item('item7', 'Лимонад'),
+      new Item('item8', 'Мята'),
+      new Item('item9', 'Мороженое'),
+      new Item('item10', 'Вишнёвый сироп'),
+      new Item('item11', 'Желток'),
+      new Item('item12', 'Вода'),
+      new Item('item13', 'Шоколад'),
+      new Item('item14', 'Арбуз'),
+    ],
+    recipes = [
+      new Recipe(
+        'recipe0',
+        'Рецепт коктейля "Тропический"',
+        ['Молоко', 'Банан', 'Апельсин', 'Сахар', 'Лёд'],
+        new Item('item', 'коктейль "Тропический"')
+      ),
+      new Recipe(
+        'recipe1',
+        'Рецепт коктейля "Мохито" безалкогольный',
+        ['Лайм', 'Лимонад', 'Сахар', 'Мята', 'Лёд'],
+        new Item('item', 'коктейль "Мохито" безалкогольный')
+      ),
+      new Recipe(
+        'recipe2',
+        'Рецепт коктейля "Айсберг в океане"',
+        ['Мороженое', 'Молоко', 'Вишнёвый сироп', 'Желток'],
+        new Item('item', 'коктейль "Мохито" безалкогольный')
+      ),
+      new Recipe(
+        'recipe3',
+        'Рецепт коктейля "Холодный банановый латте"',
+        ['Банан', 'Молоко', 'Вода', 'Шоколад', 'Лёд'],
+        new Item('item', 'коктейль "Мохито" безалкогольный')
+      ),
+      new Recipe(
+        'recipe4',
+        'Рецепт коктейля "Арбузный лимонад"',
+        ['Арбуз', 'Лайм', 'Лимонад', 'Мята', 'Лёд'],
+        new Item('item', 'коктейль "Мохито" безалкогольный')
+      ),
+      new Recipe(
+        'recipe5',
+        'Рецепт коктейля "Мятное молоко"',
+        ['Молоко', 'Сахар', 'Мята'],
+        new Item('item', 'коктейль "Мохито" безалкогольный')
+      ),
+    ]
+  ) {
+    super();
+    this.items = items;
+    this.recipes = recipes;
+    this.idCounter = this.items.length + this.recipes.length;
+  }
+
+  getRecipe(id) {
+    return this.recipes.find(recipe => recipe.id === id);
+  }
+
+  getItem(id) {
+    return this.items.find(item => item.id === id);
+  }
+
+  /* deleteElement(ev) {
+    const data = ev.dataTransfer.getData('text');
+    if (data.replace(/[^a-z]/g, '') === 'item') {
+      const itemsId = this.items.map(value => value.id);
+      this.items.splice(itemsId.indexOf(data), 1);
+    } else {
+      const recipesId = this.recipes.map(value => value.id);
+      this.recipes.splice(recipesId.indexOf(data), 1);
+    }
+  } */
+
+  craftItem(recipeId, ingredients = []) {
+    let result = null;
+    const recipe = this.getRecipe(recipeId);
+    if (
+      ingredients.every(item => recipe.ingredients.includes(this.getItem(item).name)) &&
+      ingredients.length === recipe.ingredients.length
+    ) {
+      ({ result } = recipe);
+      result.id = `item${(this.idCounter += 1)}`;
+      this.items.push(result);
+    }
+    return result;
+  }
+
+  craftRecipe(recipeName, itemName, ingredients = []) {
+    let result = this.items.find(item => item.name === itemName);
+    if (result === undefined) result = new Item(null, itemName);
+    const newRecipe = new Recipe(`recipe${(this.idCounter += 1)}`, recipeName, ingredients, result);
+    this.recipes.push(newRecipe);
+    return newRecipe;
+  }
+}
+export default Model;
