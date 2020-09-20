e7fe96acab5bd3d6db5f7131bfa84da2a2a340fa
VladislavStankevich97 <ctahok111@gmail.com>
2020-09-20 18:11:45 +0300

    done

--- src/controller.js
+++ src/controller.js
@@ -4,22 +4,12 @@ class Controller {
     this.view = view;
 
     view.on('craftItem', this.craftItem.bind(this));
-    view.on('craftRecipe', this.craftRecipe.bind(this));
     view.show(model.items, model.recipes);
   }
 
   craftItem({ recipe, items }) {
     const item = this.model.craftItem(recipe.id, Array.from(items).map(value => value.id));
     this.view.craftItem(item);
-  }
-
-  craftRecipe({ recipeName, itemName, items }) {
-    const recipe = this.model.craftRecipe(
-      recipeName,
-      itemName,
-      Array.from(items).map(value => value.innerText)
-    );
-    this.view.craftRecipe(recipe);
   }
 }
 export default Controller;
